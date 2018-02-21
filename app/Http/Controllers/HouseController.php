a12b8d8f46451e4eac28335c7228c9c82ecb3498
Apple <apple@bibi.local>
2018-02-21 21:02:16 +0800

    commisstioned optimazotion

--- app/Http/Controllers/HouseController.php
+++ app/Http/Controllers/HouseController.php
@@ -638,17 +638,155 @@ pow(sin( ($lng*pi()/180-JSON_EXTRACT(location_info, '$.lng')*pi()/180)/2),2)))*1
         $id = request('id');
         $count = request('count') ?: 1;
 
+
+        $titles = [
+            'Designer Loft Bangkok Silom',
+            '地理位置便利,1 分钟可达 BTS 空铁,提供 WiFi',
+            '3BR Suite 146sqm BTS Thonglo',
+            'GARDEN IN BANGKOK B&B',
+            '5分钟到旺角!新装宽敞的房间! New & Cozy Rm! 5mins to Mongkok !4',
+            'New & Comfy Private Room for Single Traveler @TST',
+            '香港 Hong Kong 長洲 Cheung Chau lsland',
+            'Bunk Bed Room #10 Tsim Sha Tsui MTR Exit B1',
+            '性價比高舒適安全的房間 - 只限女生',
+            '5分钟到旺角!新装宽敞的房间! New & Cozy Rm! 5mins to Mongkok !5',
+            'Private OpenStudio w/ terrace @ Mid-Levels Central',
+            '双人床房间,步行十多步到地铁站、巴士站,能说普通话、广东话和英语。',
+            'Cozy Twin Room@Mong Kok City Centre 旺角區舒適標雙房',
+            '单人床房间,步行十多步到地铁站、巴士站,能说普通话、广东话和英语。',
+            '★7)Hot Deal! 1min from Tsim Sha Tsui metro',
+            'Cebu Hotel Deluxe Single Room #2 Ensuite Bathroom',
+            '(808) Double bed with ensuite bath - 3 mins mTR',
+            '市中心三大商业广场环绕,独立房间超大2米床',
+            '广州南沙区三民岛/百万园/19冲湿地公园',
+            '趣舍,市中心科大,独立院落Loft别墅中的一间,共5间,全部独立卫生间',
+            '回到家的感觉',
+            '长江水世界旁 高尔夫花园 北欧风格三居 万科柏悦湾桃源居 comfy home away home',
+            '设计师之家/日式大户型/拍照佳景(两房) Designers ( Home2 bedrooms )',
+            'Perry的舒适大床房',
+            '中山东区全新日式+欧式家庭公寓',
+            '婚房！婚房！结婚啦！有了她就有可以有老婆了！视野好',
+            '离梦想很近，实现舒适三房奢华生活，新婚首选',
+            '精致三房，尚的装修风格，婚房优选',
+            '13年在观望，14年在观望！15年您还在观望么？机会只有一次！',
+            '120万买一套数一数二的学区房，还等什么？',
+            '观望是没有结果的，该出手时就出手，这才是硬道理！',
+            '送阁楼，相信独具慧眼的您会买就会赚，仅此一套！',
+            '投资少，回报高的性价好房出售！',
+            '忍痛急售！！XX对面即将拆迁的房子，拆迁后绝对赚到！',
+            '什么叫真正特价房看看这套你会明白！急售',
+            '好的房子不是用来欣赏的是用来享受的！！！',
+            '政策来了您犹豫了，低价房来了您心动吗？',
+            '带实验小学名额，别让我们孩子输在起跑线上',
+            '精致户型，享受无限阳光生活。',
+            '精品学区房！低价急售！广场规划中!各自付税！',
+            '全明户型，黄金楼层阳光好，交通便利，设施齐全',
+            '双学区！豪装好楼层！三房无税好房',
+            '优质学区，尽可享受优质的教育。',
+            '经典三房！东边户型！全明！精装修！两证齐全！',
+            '新+阳光+好户型+双南全明+靠近XX公园',
+            '高档小区人文荟萃高品质的象征豪华婚装！',
+            '震撼价！阳光城，精装新房卖毛坯价，要买快来看！',
+            '精品好房，适合年经人首次购房，总价低',
+            '真正的景观好房动静结合悠然自得！！！',
+            '实木地板三房无税！楼层好超低价交通便利',
+            '反季节买带暖气好房，图的就是性价比好',
+            '立体交通全程对接，主城生活唾手可得',
+            '多维度交通，10分钟到地铁，大型超市旁'
+        ];
+
+
+        $communitys = [
+            '帕特万',
+            '拉敏特拉',
+            '某某厂居民小区',
+            '幸福小区',
+            '燕园',
+            '长乐渡',
+            '幸福里9号',
+            '诗房村',
+            '清华园 ',
+            '和风细宇',
+            '金成维也纳',
+            '金成维也纳春天',
+            '御墅邻峰',
+            '君悦海棠',
+            '塞纳河畔',
+            '山水天域',
+            '爱情公寓',
+            '紫崧枫林上城',
+            '南湖清水世家',
+            '海上五月花',
+            '爱佳798',
+            '格林小城',
+            '巴塞玫瑰城',
+            '中央华府',
+            '都市家园',
+            '梧桐树下',
+            '佳境天城',
+            '汾水尚苑',
+            '梅苑小区',
+            '高巢',
+            '金汇花园',
+            '新明星花园',
+            '白玉兰花园',
+            '莫奈印象'
+        ];
+
+        $covers = [
+            'cover_1.jpg',
+            'PH_case-study-perpetuum-smart-home.jpg',
+            'Vantage Rotator.jpg',
+            'MaxHan.jpg',
+            'home-design-contemporary2.jpg',
+            'slide3.jpg',
+            'hem-home-002.jpg',
+            'coolum_geneva-facade-copy.jpg',
+            'ihg-home-brand-gallery-cv.jpeg',
+            '56587_11491_001.jpg',
+            'adina-melbourne-flinders-street-apartment-hotel-one-and-two-bedroom-apartment-2-2013.jpg',
+            'upscale.jpg',
+            'feat4.jpg',
+            'architecture-modern-apartment-1-850x450.jpg',
+            '16210D02.jpg',
+            'first-home-starter-home-mst.jpg',
+            'open-concept-japanese-family-home-with-domed-interior-1-night-lighting.jpg',
+        ];
+
+
         $arr = [];
         for($i = 0;$i<$count;$i++){
             $clone = $this->model->find($id)->replicate();
+
+            $image = $clone->house_img;
+
             $location =$clone->location_info;
+
+            $fileName = $covers[rand(0, count($covers) - 1)];
+
+            $clone->title = $titles[rand(0,count($titles) - 1)];
+
+            $clone->community = $communitys[rand(0,count($communitys) - 1)];
+            $clone->price = rand(100,9999);
+
+            $image['cover'] = [[
+                            'name'=>$fileName,
+                            'type'=>'jpg',
+                            'size'=>true,
+                            'url'=>url("/storage/$fileName"),
+                            'get'=>url("/api/img/?file=$fileName")
+                        ]];
 
             $lng = request('lng') ?: $location['lng'];
             $lat = request('lat') ?: $location['lat'];
 
+
+
             $location['lng'] = $lng + ((mt_rand() / mt_getrandmax()) - 0.5) * 0.19;
             $location['lat'] = $lat + ((mt_rand() / mt_getrandmax()) - 0.5) * 0.19;
             $clone->location_info = $location;
+            $clone->house_img = $image;
+
             array_push($arr,$clone->save());
         }
 
